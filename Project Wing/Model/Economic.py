2bf9de78774be5214f71bd598fbcab6682f84857
Kloniphani Maluleke <kloniphani@gmail.com>
2019-09-16 13:37:26 +0200

    Capex

--- Project Wing/Model/Economic.py
+++ Project Wing/Model/Economic.py
@@ -1,5 +1,8 @@
 from pandas import *
 from numpy import * 
+
+from datetime import datetime
+
 import progressbar
 
 Scenarios = ['Mopani - UAV',	'Vhembe - UAV',	'Waterberg - UAV',	'Chris-Hani - UAV',	'Frances Baard - UAV',	'Soweto - UAV',	'Khayelitsha - UAV',	'Lulekani - UAV',	'Zeerust - UAV',	'Duduza - UAV',	'Hlankomo - UAV',	'Mandileni - UAV',	'Gon’on’o - UAV']
@@ -65,10 +68,42 @@ with progressbar.ProgressBar(max_value = len(Scenarios)) as bar:
 		IRR[Scenario] = IRRTerms
 		bar.update(Count)
 
-Columns = ['Counter'] + Scenarios + ScenariosB 
+
+ScenariosC = ['Mopani - H',	'Vhembe - H',	'Waterberg - H',	'Chris-Hani - H',	'Frances Baard - H',	'Soweto - H',	'Khayelitsha - H',	'Lulekani - H',	'Zeerust - H',	'Duduza - H',	'Hlankomo - H',	'Mandileni - H',	'Gon’on’o - H']
+
+CAPEX = [7748855.1,8873203.95,4494657.6,5835005.1,2590017.3,15940710.25,5177777.85,88218.8,57182.2,951132,2010.35,2147.85,4130.7]
+
+OPEX = [4013087.314,4756181.423,2496568.726,2923089.579,1404675.342,14947186.34,4604529.564,81512.87412,54973.79883,3480.75,7.65,7.65,15.3]
+
+Count = 0
+NumberOfTerms = 10
+with progressbar.ProgressBar(max_value = len(Scenarios)) as bar:
+	for Scenario in ScenariosC:
+		Terms = []	
+		IRRTerms = []
+		for fee in DATA['Counter']:
+			CashFlow = []
+			for term in range(NumberOfTerms):
+				CashFlow.append((Nu[Count] * 12 * float(fee/1000)) - OPEX[Count])
+			Terms.append(npv(Rate, CashFlow) - CAPEX[Count])
+
+			Temp = []; Temp.append((CAPEX[Count]*-1)); 
+			IRRTerms.append(irr(Temp + CashFlow))
+			#print("{0:10} - Fee: {1:4} CASHFLOW: {2} NPV: {3:.2f} CAPEX: {4:.2f}".format(Scenario, fee, CashFlow[0], (npv(Rate, CashFlow) - CAPEX[Count]), CAPEX[Count]))
+	
+		Count += 1 
+		DATA[Scenario] = Terms
+		IRR[Scenario] = IRRTerms
+		bar.update(Count)
+
+
+
+Columns = ['Counter'] + Scenarios + ScenariosB + ScenariosC
 DataFrames = DataFrame(data = DATA, columns = Columns) #Create a Pandas dataframe from some data.
 IRRDataFrames = DataFrame(data = IRR, columns = Columns)
-Writer = ExcelWriter('./Model/Computed Data/Model.xlsx', engine='xlsxwriter') #Create a Pandas Excel writer using XlsxWriter as the engine.
+
+
+Writer = ExcelWriter('./Model/Computed Data/Model-{0}.xlsx'.format(datetime.now().strftime('%Y-%m-%d %H-%M')), engine='xlsxwriter') #Create a Pandas Excel writer using XlsxWriter as the engine.
 DataFrames.to_excel(Writer, index=False, sheet_name='NPV')
 IRRDataFrames.to_excel(Writer, index=False, sheet_name='IRR')
 Writer.save()
